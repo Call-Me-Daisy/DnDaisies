2f33d91183c75f5e958fcfa8779d44b92a3b4a07
TDCalverley99 <TDCalverley99@gmail.com>
2022-01-21 18:52:40 +0000

    Expanded user capabilities. Added (naive) user feedback

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ require("dotenv").config();
 const { Client, Intents, MessageAttachment} = require("discord.js");
 const {createCanvas, loadImage} = require ("canvas");
 const {DaisyMap, DaisyChar} = require("./map");
+//const HELP = require("./help");
 
-//--------------------------------------------------------------------
+//--------------------------------------------------------------------CONSTANTS
 const bot = new Client({intents:[
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
@@ -14,78 +15,124 @@ const bot = new Client({intents:[
 });
 
 const PREFIX = "--";
-const MAX_PW = 1080, MAX_PH = 800;
 const allMaps = new Map();
-//--------------------------------------------------------------------
+
+//--------------------------------------------------------------------HELPERS
 function capitalCase(str) {
 	return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
-function parseCoords(coord) {
-	return [parseInt(coord.slice(1),10), coord.charCodeAt(0)-64];
+function sendTo(id, message) {
+	return bot.channels.cache.get(id).send(message);
 }
-//--------------------------------------------------------------------
+function sendTemp(id, message, duration = 5000) {
+	sendTo(id, message).then(msg => {
+		setTimeout(() => msg.delete(), duration);
+	});
+}
+//--------------------------------------------------------------------DECLUTTERING
+//------------------------------------DO
 function doQuit(id, command) {
 	setTimeout(function() {bot.destroy();}, 1500);
 }
 function doPing(id, command) {
-	bot.channels.cache.get(id).send("pong\t(" + (Date.now()-message.createdTimestamp).toString() + "ms)");
+	sendTo(id, "pong\t(" + (Date.now()-message.createdTimestamp).toString() + "ms)");
 }
 function doNew(id, command) {
-	allMaps.set(id, new DaisyMap(parseInt(command[1],10), parseInt(command[2],10), MAX_PW, MAX_PH));
+	while(command.length < 5) {command.push(undefined);}
+	allMaps.set(id, new DaisyMap(command[1], command[2], command[3], command[4]));
 }
 function doHide(id, command) {
 	allMaps.get(id).getChar(command[1]).visible = !allMaps.get(id).getChar(command[1]).visible;
 }
 function doAdd(id, command) {
-	allMaps.get(id).addChar(capitalCase(command[2]), new DaisyChar(command[1], parseCoords(command[3]), true));
+	allMaps.get(id).addChar(capitalCase(command[2]), new DaisyChar(command[1], command[3], true));
 }
 function doAddMany(id, command) {
+	const thisMap = allMaps.get(id);
+	const charName = capitalCase(command[2]);
 	command[3].split(",").forEach((coord, i) => {
-		allMaps.get(id).addChar(capitalCase(command[2]), new DaisyChar(command[1], parseCoords(coord), true));
+		thisMap.addChar(charName, new DaisyChar(command[1], coord, true));
 	});
 }
+function doAddArea(id, command) {
+	allMaps.get(id).addArea(command[1], command[2]);
+}
 function doMove(id, command) {
-	allMaps.get(id).getChar(command[1]).moveTo(parseCoords(command[2]));
-	if (allMaps.get(id).getChar(command[1]).visible) {doMap(id, command);}
+	const char = allMaps.get(id).getChar(capitalCase(command[1]))[command[2]];
+	char.moveTo(command[3]);
+	//if (char.visible) {doMap(id, command);}
 }
 async function doMap(id, command) {
-	bot.channels.cache.get(id).send({
+	sendTo(id, {
 		files: [new MessageAttachment(await allMaps.get(id).buildMap(), id + "_map.png")]
 	})
 }
-
-//--------------------------------------------------------------------
+//------------------------------------TRY
+function tryNew(id, command) {
+	try {doNew(id, command);} catch (err) {
+		sendTemp(id, "Error. Please format as follows:\n\t--new [letter][number]")
+	}
+}
+function tryHide(id, command) {
+	try {doHide(id, command);} catch (err) {
+		sendTemp(id, "Error. Please format as follows:\n\t--hide [name]")
+	}
+}
+function tryAdd(id, command) {
+	try {doAdd(id, command);} catch (err) {
+		sendTemp(id, "Error. Please format as follows:\n\t--add [party/enemy/neutral/object] [name] [letter][number]")
+	}
+}
+function tryAddMany(id, command) {
+	try {doAddMany(id, command);} catch (err) {
+		sendTemp(id, "Error. Please format as follows:\n\t--addmany [name] [letter][number],[letter][number],...")
+	}
+}
+function tryAddArea(id, command) {
+	try {doAddArea(id, command);} catch (err) {
+		sendTemp(id, "Error. Please format as follows:\n\t--addarea [object/background/wall] [r4:ng3],[r4:ng3],...");
+	}
+}
+function tryMove(id, command) {
+	try {doMove(id, command);} catch (err) {
+		sendTemp(id, "Error. Please format as follows:\n\t--move [name] [letter][number]")
+	}
+}
+//--------------------------------------------------------------------MAIN
 bot.once("ready", () => {
 	console.log(`Logged in as ${bot.user.tag}`);
-	//bot.channels.cache.get(process.env.TEST_CHANNEL).send(process.env.TEST_COMMAND);
+	//sendTo(process.env.TEST_CHANNEL, process.env.TEST_COMMAND);
 });
 
 bot.on("messageCreate", (message) => {
 	if (!message.author.bot) {
+		let shouldDelete = false;
 		console.log(`#${message.author.discriminator} @${message.channel.id}`);
 
 		message.content.split("\n").forEach( async (messageLine, i) => {
 			if (messageLine.startsWith(PREFIX)) {
+				shouldDelete = true;
 				console.log(messageLine);
 				const command = messageLine.slice(PREFIX.length).split(" ");
 
 				switch (command[0].toLowerCase()) {
-					case "quit": doQuit(message.channel.id, command); break;
-					case "ping": doPping(message.channel.id, command); break;
-					case "new": doNew(message.channel.id, command); break;
-					case "hide": doHide(message.channel.id, command); break;
-					case "add": doAdd(message.channel.id, command); break;
-					case "addmany": doAddMany(message.channel.id, command); break;
-					case "move": doMove(message.channel.id, command); break;
-					case "map": await doMap(message.channel.id, command); break;
+					case "quit": doQuit(message.channel.id, command); break; //an exception here should never occur
+					//case "help": doHelp(message.channel.id, command); break; //an exception here should never occur
+					case "ping": doPing(message.channel.id, command); break; //an exception here should never occur
+					case "new": tryNew(message.channel.id, command); break;
+					case "hide": tryHide(message.channel.id, command); break;
+					case "add": tryAdd(message.channel.id, command); break;
+					case "addmany": tryAddMany(message.channel.id, command); break;
+					case "addarea": tryAddArea(message.channel.id, command); break;
+					case "move": tryMove(message.channel.id, command); break;
+					case "map": await doMap(message.channel.id, command); break; //an exception here shows a fatal error.
 
-					default: bot.channels.cache.get(message.channel.id).send(messageLine.slice(PREFIX.length));
+					default: sendTo(message.channel.id, messageLine.slice(PREFIX.length));
 				}
 			}
 		});
-		message.delete();
+		if (shouldDelete) {message.delete();}
 	}
 });
-
-//--------------------------------------------------------------------
+//--------------------------------------------------------------------FINALIZE
 bot.login(process.env.TOKEN);
--- map.js
+++ map.js
@@ -1,36 +1,67 @@
 const fs = require('fs')
 const { createCanvas, loadImage } = require('canvas')
 
+//--------------------------------------------------------------------CONSTANTS
 const TEAM = new Map([
-	["o", "#222"],
+	["b", "#a38b53"],
+	["o", "#848b94"],
+	["w", "#000"],
 	["e", "#f33"],
 	["p", "#3f3"],
 	["a", "#38f"],
 	["n", "#640"]
 ]);
 
+const MAX_PH = 1080, MAX_PV = 800;
+//--------------------------------------------------------------------HELPERS
+function parseCoords(coord) {
+	return [parseInt(coord.slice(1),10), coord.charCodeAt(0)-64];
+}
+function lesserOf(a, b) {
+	if (b<a) {return b;}
+	return a;
+}
+function parseRangeCoords(rangeCoords) {
+	if (rangeCoords === undefined) {rangeCoords = "A1:"+_dims;}
+	let out = rangeCoords.split(",");
+	out.forEach((range,i) => {
+		out[i] = range.split(":");
+		if (out[i].length < 2) {out[i].push(out[i][0]);}
+		out[i].forEach((corner,j) => {
+			out[i][j] = parseCoords(corner);
+		});
+	});
+	return out;
+}
+//--------------------------------------------------------------------MAIN
+//------------------------------------CHAR
 class DaisyChar {
 	constructor(_team, _pos, _visible = false) {
 		this.team = TEAM.get(_team[0].toLowerCase());
-		if (this.team === undefined) {throw "Invalid _team for DaisyChar!";}
+		if (this.team === undefined) {throw `Team ${_team} not valid!`;}
 
-		this.pos = _pos;
+		this.pos = parseCoords(_pos);
 		this.visible = _visible;
 	}
 
 	moveTo(_pos) {
-		this.pos = _pos;
+		this.pos = parseCoords(_pos);
 	}
 }
-
+//------------------------------------MAP
 class DaisyMap {
-	constructor(_H, _V, _pH, _pV, _pM = NaN) {
-		this.dims = [_H, _V];
-		this.pS = (_pH/_H < _pV/_V) ? Math.floor(_pH/_H) : Math.floor(_pV/_V);
-		this.pM = (isNaN(_pM)) ? Math.ceil(this.pS/64) : _pM;
+	constructor(_dims, _bg, _obj, _wall, _pH = MAX_PH, _pV = MAX_PV) {
+		this.dims = parseCoords(_dims);
+		this.pS = lesserOf(Math.floor(_pH/this.dims[0]), Math.floor(_pV/this.dims[1]));
+		this.pM = Math.ceil(this.pS/64);
 		this.chars = new Map();
 
-		this.canvas = createCanvas((1+_H)*this.pS, (1+_V)*this.pS);
+		if (_bg === undefined) {_bg = "A1:"+_dims;}
+		this.bg = parseRangeCoords(_bg);
+		this.obj = (_obj === undefined) ? [] : parseRangeCoords(_obj);
+		this.wall = (_wall === undefined) ? [] : parseRangeCoords(_wall);
+
+		this.canvas = createCanvas((1+this.dims[0])*this.pS, (1+this.dims[1])*this.pS);
 		this.context = this.canvas.getContext("2d");
 	}
 
@@ -48,12 +79,34 @@ class DaisyMap {
 		if (this.chars.get(charName) === undefined) {throw `Char ${CharName} not registered!`;}
 		return this.chars.get(charName)[0];
 	}
+	addArea(type, rangeCoords) {
+		let areaHolder;
+		switch (type[0].toLowerCase()) {
+			case "b": areaHolder = this.bg; break;
+			case "w": areaHolder = this.wall; break;
+			case "o": areaHolder = this.obj; break;
+
+			default: throw `Type ${type} not valid!`;
+		};
+		parseRangeCoords(rangeCoords).forEach((area, i) => {
+			areaHolder.push(area);
+		});
+	}
 
 	fillCell(h, v) {
 		this.context.fillRect(h*this.pS+this.pM, v*this.pS+this.pM, this.pS-2*this.pM, this.pS-2*this.pM);
 	}
 	writeCell(text, h, v) {
 		this.context.fillText(text, (2*h+1)*this.pS/2, (2*v+1)*this.pS/2);
+	}
+	fillArea(area) {
+		area.forEach((range, i) => {
+			for (let h = range[0][0]; h <= range[1][0]; h++) {
+				for (let v = range[0][1]; v <= range[1][1]; v++) {
+					this.fillCell(h,v);
+				}
+			}
+		});
 	}
 
 	prepMap() {
@@ -64,24 +117,24 @@ class DaisyMap {
 		this.context.textBaseline = "middle";
 		this.context.font = (this.pS/2-3*this.pM).toString()+"px Arial";
 
+		this.context.fillStyle = TEAM.get("b");
+		this.fillArea(this.bg);
+		this.context.fillStyle = TEAM.get("o");
+		this.fillArea(this.obj);
+		this.context.fillStyle = TEAM.get("w");
+		this.fillArea(this.wall);
+
 		this.context.fillStyle = "#fff";
-		for (let v = 1; v <= this.dims[1]; v++) {
-			for (let h = 1; h <= this.dims[0]; h++) {
-				this.fillCell(h, v);
-			}
-		}
-		this.context.fillStyle = "#fff";
-		for (let v = 1; v <= this.dims[1]; v++) {this.writeCell(String.fromCharCode(64+v), 0, v);}
 		for (let h = 1; h <= this.dims[0]; h++) {this.writeCell(h.toString(), h, 0);}
+		for (let v = 1; v <= this.dims[1]; v++) {this.writeCell(String.fromCharCode(64+v), 0, v);}
 	}
 
 	buildMap() {
 		this.prepMap();
 
-		var charCode, many;
 		for (const [charName, charLs] of this.chars.entries()) {
-			charCode = charName[0] + charName[charName.length-1];
-			many = (charLs.length > 1);
+			let charCode = charName[0] + charName[charName.length-1];
+			let many = (charLs.length > 1);
 
 			charLs.forEach((char, i) => {
 				if (char.visible) {
@@ -96,7 +149,7 @@ class DaisyMap {
 		return this.canvas.toBuffer("image/png");
 	}
 }
-
+//--------------------------------------------------------------------FINALIZE
 export {
 	DaisyMap,
 	DaisyChar
