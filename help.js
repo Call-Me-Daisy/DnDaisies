a54821db75b31bb1d25d65e611ec1c2e63529bfe
TDCalverley99 <TDCalverley99@gmail.com>
2022-02-06 14:09:14 +0000

    Bugfixing & Usability

--- help.js
+++ help.js
@@ -4,19 +4,20 @@ For more information on a command, do '${prefix}help [command]'
 Arguments shown with an asterisk are optional
 **Commands:**
 ${prefix}ping = Check speed
+${prefix}list = Display the computer name of each token on the map
 ${prefix}new = Create new map
-${prefix}add = Add new character to current map
-${prefix}addMany = Add many identical characters to current map
+${prefix}add = Add new token to current map
+${prefix}addMany = Add many identical tokens to current map
 ${prefix}addArea = Colour in any number of ranges as static objects
-${prefix}hide = Hide a named character
-${prefix}move = Move a named character to a new coordinate
+${prefix}hide = Hide a particular token
+${prefix}move = Move a particular token to a new coordinate
 ${prefix}map = Generate and send map image
 **Terms:**
 'coordinate/coord' = [Letter][Number], ie. A1, Z7; NOT 7Z
 'range/area' = [top-left coord]:[bottom-right coord], ie. B15:H19; NOT B19:H15
 'CSV' = A comma-seperated list (NO spaces), ie. A1,B7,G8
 **Notes:**
-Only co-ordinates&ranges should be case sensitive; any such bugs should be reported
+Only co-ordinates&ranges should be case sensitive; please report a bug if this is not the case.
 	`;
 }
 function helpDefault(prefix) {
@@ -32,43 +33,67 @@ Check current speed of application.
 **No arguments**
 	`;
 }
+function helpList(prefix) {
+	return `
+List the reference name of each token.
+**Aliases:** ${prefix}list ${prefix}tokens
+**Arguments:**
+1. Which team to list (empty => all teams)
+	`;
+}
 function helpNew(prefix) {
 	return `
-Replace current map with empty map of given size; will fill with background if no areas are specified.
+Replace current map with empty map of given size
 **Aliases:** ${prefix}new ${prefix}newmap
 **Arguments:**
-1* One co-ordinate representing the bottom-right corner pf the map (top-left = 'A1
-2. CSV of co-ordinates or ranges to paint as the background
-3. As 2 for static objects
-4. As 2 for walls (removes background; can be faster depending on map layout
+1* One co-ordinate representing the bottom-right corner of the map (top-left = 'A1')
+2. CSV of co-ordinates or ranges to paint as the background (empty => entire map)
+3. As 2 for static objects (empty => none)
+4. As 2 for walls (overrides background; empty => none)
 	`;
 }
 function helpHide(prefix) {
 	return `
-Hide or Reveal the given creature.
+Hide or Reveal the given token.
 **Aliases:** ${prefix}hide ${prefix}reveal
 **Arguments:**
-1* Name of creature to hide/reveal
-2. Number of creature to hide/reveal; leave blank [for all/if no number]
+1* Name & Number of token to hide/reveal (ie. Jeff, Zombie_4)
 	`;
 }
 function helpAdd(prefix) {
 	return `
-Create single character in the given co-ordinate.
+Create single token in the given co-ordinate.
 **Aliases:** ${prefix}add ${prefix}newtile
 **Arguments:**
-1* Team of character being added (party/enemy/neutral/object) - only actually need first letter
-2* Name of character being added
-3* Coordinate to place character when added
+1* Team of token being added (party/enemy/neutral/object) - only actually need first letter
+2* Name (without number!) of token being added
+3* Coordinate to place token when added
 	`;
 }
-function helpAddMany(prefix) {
+function helpCopy(prefix) {
 	return `
-Create multiple identical creatures in the given co-ordinates.
+Add copies of the given token in the given co-ordinates.
+**Aliases:** ${prefix}copy ${prefix}duplicate ${prefix}reinforce
+**Arguments:**
+1* Name (without number!) of the token being copied
+2* CSV of co-ordinates in which to create copies
+	`;
+}
+function helpRemove(prefix) {
+	return `
+Completely remove a token from the map.
+**Aliases:** ${prefix}remove ${prefix}delete ${prefix}kill
+**Arguments:**
+1* Name & Number of token being removed (ie. Jeff, Zombie_4, Iron-Golem_2)
+	`;
+}
+function helpAddGroup(prefix) {
+	return `
+Create multiple identical tokens in the given co-ordinates.
 **Aliases:** ${prefix}addmany ${prefix}newgroup ${prefix}newtiles
 **Arguments:**
-1* Team of creatures being added (party/enemy/neutral/object) - only actually need first letter
-2* Name of creatures being added
+1* Team of tokens being added (party/enemy/neutral/object) - only actually need first letter
+2* Name (without number!) of tokens being added
 3* CSV of coordinates (not ranges) to place them
 	`;
 }
@@ -83,12 +108,20 @@ Mark areas of the map as the given type of static objects.
 }
 function helpMove(prefix) {
 	return `
-Move a creature to a new co-ordinate.
-**No Aliases**
+Move a token to a new co-ordinate.
+**No Aliases:** ${prefix}move ${prefix}movetoken
 **Arguments:**
-1* Name of creature to move
+1* Name & Number of token to move (ie. Jeff, Zombie_4)
 2* Coordinate to move it to
-3. Number of creature; leave blank for no number
+	`;
+}
+function helpMoveGroup(prefix) {
+	return `
+Move multiple tokens in a group to new co-ordinates
+**Aliases:** ${prefix}movegroup ${prefix}movetokens
+**Arguments:**
+1* Name (without number!) of tokens to move
+2* ORDERED CSV of Coordinates to move them to (first creature -> first co-ord, etc.)
 	`;
 }
 function helpMap(prefix) {
@@ -101,26 +134,35 @@ Display this channel's map (created with *prior** calls to the other functions).
 
 function helpSwitch(prefix, command) {
 	if (command.length == 1) {return helpMain(prefix);}
-	else {
-		switch (command[1].toLowerCase(prefix)) {
-			case "ping": return helpPing(prefix);
-			case "newmap":
-			case "new": return helpNew(prefix)
-			case "reveal":
-			case "hide": return helpHide(prefix);
-			case "newtile":
-			case "add": return helpAdd(prefix);
-			case "newgroup":
-			case "newtiles":
-			case "addmany": return helpAddMany(prefix);
-			case "newarea":
-			case "addarea": return helpAddArea(prefix);
-			case "move": return helpMove(prefix);
-			case "display":
-			case "map": return helpMap(prefix);
+	switch (command[1].toLowerCase(prefix)) {
+		case "ping": return helpPing(prefix);
+		case "tokens":
+		case "list": return helpList(prefix);
+		case "newmap":
+		case "new": return helpNew(prefix);
+		case "reveal":
+		case "hide": return helpHide(prefix);
+		case "newtoken":
+		case "add": return helpAdd(prefix);
+		case "reinforce":
+		case "duplicate":
+		case "copy": return helpCopy(prefix);
+		case "kill":
+		case "delete":
+		case "remove": return helpRemove(prefix);
+		case "newgroup":
+		case "addmany":
+		case "addgroup": return helpAddGroup(prefix);
+		case "newarea":
+		case "addarea": return helpAddArea(prefix);
+		case "movetoken":
+		case "move": return helpMove(prefix);
+		case "movetokens":
+		case "movegroup": return helpMoveGroup(prefix);
+		case "display":
+		case "map": return helpMap(prefix);
 
-			default: return helpDefault(prefix);
-		}
+		default: return helpDefault(prefix);
 	}
 }
 
