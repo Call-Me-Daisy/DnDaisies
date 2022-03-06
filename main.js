9dd091bf84f7aacc3bd9f9e7df00f2656212baf3
TDCalverley99 <TDCalverley99@gmail.com>
2022-03-06 19:41:19 +0000

    Temp for use

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const {Client, Intents, MessageAttachment, ThreadChannel} = require("discord.js");
-const {createCanvas, loadImage} = require ("canvas");
-const {TEAM, DaisyMap, DaisyChar} = require("./map");
+const {createCanvas, loadImage} = require("canvas");
+const {TEAM, DaisyMap, DaisyChar, Arena, PaintStyle} = require("./map");
 const {helpSwitch} = require("./help");
 
 //--------------------------------------------------------------------GLOBALS
@@ -14,7 +14,7 @@ const bot = new Client({intents:[
     Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
 ]});
 
-const allMaps = new Map();
+const allArenas = new Map();
 
 const HELPTIMER = 5000;
 const PREFIX = "--";
@@ -44,11 +44,11 @@ function buildLinks(message) {
 		m: message
 	};
 }
-function fetchMap(links) {
-	return allMaps.get(links.c.id);
+function fetchArena(links) {
+	return allArenas.get(links.c.id);
 }
 
-function sendTo(links, content) {
+function sendTo(links) {
 	return links.c.send(content);
 }
 function makeTemp(message, duration = HELPTIMER) {
@@ -57,37 +57,37 @@ function makeTemp(message, duration = HELPTIMER) {
 	});
 }
 
-function clearImg(dMap) {
-	if (dMap.img) {
-		deleteFrom(dMap.img.parentId, dMap.img.id);
-		dMap.img.delete().catch((error) => {});
-		dMap.img = false;
+function clearImg(arena) {
+	if (arena.img) {
+		deleteFrom(arena.img.parentId, arena.img.id);
+		arena.img.delete().catch((error) => {});
+		arena.img = false;
 	}
 }
-function clearMap(dMap) {
-	if (dMap.map) {
-		dMap.map.delete().catch((error) => {});
-		dMap.map = false;
+function clearMap(arena) {
+	if (arena.map) {
+		arena.map.delete().catch((error) => {});
+		arena.map = false;
 	}
 }
 //--------------------------------------------------------------------DECLUTTERING
 function doQuit() {//TEMP: For cleaning between updates
-	for (const [id, dMap] of allMaps) {
-		clearMap(dMap);
-		clearImg(dMap);
+	for (const [id, arena] of allArenas) {
+		clearMap(arena);
+		clearImg(arena);
 	}
 	setTimeout(function() {bot.destroy();}, HELPTIMER*1.5);
 }
 function doLog(links, command) {//TEMP: Debug helper
-	if (command.size !== 1) {
+	if (links.m.reference !== null) {
+		fetchReference(links.m).then((msg) => {console.log(msg)});
+	}
+	else if (command.size !== 1) {
 		switch (command[1][0].toLowerCase()) {
 			case "m": console.log(links.m); break;
 			case "c": console.log(links.c); break;
 			case "g": console.log(links.g); break;
 		}
-	}
-	else if (links.m.reference !== null) {
-		fetchReference(links.m).then((msg) => {console.log(msg)});
 	}
 }
 function doHelp(links, command) {
@@ -104,7 +104,7 @@ function doList(links, command) {
 		for (const [key, teamTup] of TEAM) {
 			msgMap.set(teamTup[0], [`__Team: ${teamTup[1]}__`]);
 		}
-		for (const [key, char] of fetchMap(links).chars) {
+		for (const [key, char] of fetchArena(links).chars) {
 			msgMap.get(char[0].team).push(`Token: ${DaisyChar.makeCharCode(key)} => Name: ${key}`);
 		}
 		let msg = [];
@@ -121,31 +121,31 @@ function doList(links, command) {
 }
 function doNew(links, command) {
 	try {
-		const [map, img] = DaisyMap.recover(fetchMap(links));
-		while(command.length < 5) {command.push(undefined);}
-		allMaps.set(links.c.id, new DaisyMap(map, img, command[1], command[2]));
+		const [map, img] = Arena.recover(fetchArena(links));
+		allArenas.set(links.c.id, new DaisyMap(map, img, command[1], command[2], TsendTemp, links));
 	} catch (e) {
 		doHelp(links, ["","new"]);
 	}
 }
 function doHide(links, command) {
 	try {
-		const char = fetchMap(links).getChar(command[1])
+		const char = fetchArena(links).getToken(TcapicalCase(command[1]));
 		char.visible = !char.visible;
 	} catch (e) {
 		doHelp(links, ["hide"]);
 	}
 }
+
 function doAdd(links, command) {
 	try {
-		fetchMap(links).addChar(DaisyChar.toKeyCase(command[2]), new DaisyChar(command[1], command[3], true));
+		fetchArena(links).addChar(DaisyChar.toKeyCase(command[2]), new DaisyChar(command[1], command[3], true));
 	} catch (e) {
 		doHelp(links, ["","add"]);
 	}
 }
 function doAddGroup(links, command) {
 	try {
-		const thisMap = fetchMap(links);
+		const thisMap = fetchArena(links);
 		const charName = DaisyChar.toKeyCase(command[2]);
 		for (const coord of command[3].split(",")) {
 			thisMap.addChar(charName, new DaisyChar(command[1], coord, true));
@@ -156,15 +156,16 @@ function doAddGroup(links, command) {
 }
 function doAddArea(links, command) {
 	try {
-		fetchMap(links).addArea(command[1], command[2]);
+		fetchArena(links).addArea(command[1], command[2]);
 	} catch (e) {
 		doHelp(links, ["","addarea"]);
 	}
 }
+
 function doCopy(links, command) {
 	try {
 		let charStr = DaisyChar.toKeyCase(command[1]);
-		const thisMap = fetchMap(links);
+		const thisMap = fetchArena(links);
 		const parent = thisMap.getChar(charStr);
 		charStr = DaisyChar.getCharTup(charStr)[0];
 		for (const coord of command[2].split(",")) {
@@ -176,7 +177,7 @@ function doCopy(links, command) {
 }
 function doRemove(links, command) {
 	try {
-		const thisMap = fetchMap(links);
+		const thisMap = fetchArena(links);
 		const charStr = DaisyChar.toKeyCase(command[1]);
 
 		let allRemoved = true;
@@ -191,14 +192,14 @@ function doRemove(links, command) {
 }
 function doMove(links, command) {
 	try {
-		fetchMap(links).getChar(DaisyChar.toKeyCase(command[1])).moveTo(command[2]);
+		fetchArena(links).getChar(DaisyChar.toKeyCase(command[1])).moveTo(command[2]);
 	} catch (e) {
 		doHelp(links, ["","move"]);
 	}
 }
 function doMoveGroup(links, command) {
 	try {
-		const charLs = fetchMap(links).getCharLs(DaisyChar.toKeyCase(command[1]));
+		const charLs = fetchArena(links).getCharLs(DaisyChar.toKeyCase(command[1]));
 		let numRemoved = 0;
 		for (const [i, coord] of command[2].split(",").entries()) {
 			while (i+numRemoved < charLs.length && charLs[i+numRemoved].removed) {numRemoved++;}
@@ -219,11 +220,11 @@ function doPing(links) {
 
 async function doImage(links) {
 	try {
-		if (fetchMap(links) === undefined) {doNew(links, ["","A1"]);}
-		let dMap = fetchMap(links);
-		if (!dMap.img) {
+		if (fetchArena(links) === undefined) {doNew(links, ["","A1"]);}
+		let arena = fetchArena(links);
+		if (!arena.img) {
 			let prompt = await sendTo(links, process.env.THREADPROMPT);
-			dMap.img = await prompt.startThread({
+			arena.img = await prompt.startThread({
 				name: process.env.THREADNAME,
 				autoArchiveDuration: 60
 			});
@@ -234,7 +235,7 @@ async function doImage(links) {
 				let messages = await bot.channels.cache.get(msg.id).messages.fetch({limit:100});
 				for (const [key, msg] of messages) {
 					if (msg.attachments.size > 0) {
-						dMap.img.send({
+						arena.img.send({
 							content: msg.content,
 							files: [msg.attachments.entries().next().value[1]]
 						});
@@ -242,12 +243,12 @@ async function doImage(links) {
 				}
 			}
 		}
-		else if (links.t && links.t.id === dMap.img.id) {
-			clearImg(dMap);
+		else if (links.t && links.t.id === arena.img.id) {
+			clearImg(arena);
 		}
 		else {
-			dMap.img.setArchived(false);
-			fetchMessage(links.c.id, dMap.img.id).then((msg) => {
+			arena.img.setArchived(false);
+			fetchMessage(links.c.id, arena.img.id).then((msg) => {
 				makeTemp(msg.reply("bump"));
 			});
 		}
@@ -257,18 +258,71 @@ async function doImage(links) {
 }
 async function doMap(links) {
 	try {
-		let dMap = fetchMap(links);
-		if (dMap.map) {clearMap(dMap);}
-		dMap.map = await sendTo(links, {
-			files: [new MessageAttachment(await dMap.buildMap(), links.c.id + "_map.png")]
+		let arena = fetchArena(links);
+		if (arena.map) {clearMap(arena);}
+		arena.map = await sendTo(links, {
+			files: [new MessageAttachment(await arena.builarena(), links.c.id + "_map.png")]
 		});
 	} catch (e) {
 		doHelp(links, ["","map"]);
 	}
 }
 //--------------------------------------------------------------------TESTING
-async function doTest(links, command) {
+function TparseNumCoord(_coordSplit) {
+	return [parseInt(_coordSplit[1],10), parseInt(_coordSplit[0],10)];
+}
+function TparseStrCoord(_coord) {
+	return [parseInt(_coord.slice(1),10), _coord.charCodeAt(0)-64];
+}
+function TparseCoord(_coord) {
+	let coordSplit = _coord.split("-");
+	return (coordSplit.length > 1) ? TparseNumCoord(coordSplit) : TparseStrCoord(_coord);
+}
 
+function TsendTemp(_links, _content, _duration = HELPTIMER) {
+	makeTemp(sendTo(_links, _content), _duration);
+}
+
+function TcapitalCase(_name) {
+	return _name[0].toUpperCase() + _name.slice(1).toLowerCase();
+}
+
+async function doTest(links) {
+	allArenas.set(links.c.id, new Arena(false, false /*await fetchChannel("948183116869173328")*/, [18,18], TsendTemp, links));
+
+	let arena = fetchArena(links);
+	let coords;
+
+	arena.newGroup("Background", PaintStyle.GRID, TEAM.get("b")[0], [18,18], TparseCoord("1-1"));
+
+	arena.newGroup("Allen", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("I7"));
+	arena.newGroup("Beauregard", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("J9"));
+	arena.newGroup("Void", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("J10"));
+	arena.newGroup("Dante", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("K11"));
+	arena.newGroup("Creida", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("L5"));
+	arena.newGroup("Berrow", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("L7"));
+	arena.newGroup("Matilda", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("L9"));
+	arena.newGroup("Galeti", PaintStyle.FILL, TEAM.get("p")[0], [1,1], TparseCoord("L10"));
+
+	coords = [];
+	for (const coord of ["E4","F5"]) {coords.push(TparseCoord(coord));}
+	arena.newGroup("Pepper", PaintStyle.IMAGE, TEAM.get("e")[0], [1,1], coords);
+
+	coords = [];
+	for (const coord of ["G7","H5","H6","H7","H8","I6"]) {coords.push(TparseCoord(coord));}
+	arena.newGroup("Crumpet", PaintStyle.IMAGE, TEAM.get("e")[0], [1,1], coords);
+
+	arena.newGroup("Cheese", PaintStyle.FILL, TEAM.get("e")[0], [2,2], TparseCoord("J6"));
+
+	coords = [];
+	for (const coord of ["J8","J11","K12"]) {coords.push(TparseCoord(coord));}
+	arena.newGroup("Iron-Chef", PaintStyle.FILL, TEAM.get("e")[0], [1,1], coords);
+
+	arena.newGroup("Fire", PaintStyle.NAMED_GRID, "#ff7733", [3,3], [12,12]);
+
+	TsendTemp(links, {
+		files: [new MessageAttachment(await arena.builarena(), links.c.id + "_map.png")]
+	});
 }
 //--------------------------------------------------------------------MAIN
 async function mainSwitch(links, command) {
--- map.js
+++ map.js
@@ -1,6 +1,4 @@
-const fs = require('fs')
 const { createCanvas, loadImage} = require('canvas')
-
 //--------------------------------------------------------------------CONSTANTS
 const TEAM = new Map([
 	["p", ["#3f3", "Party"]],
@@ -11,6 +9,7 @@ const TEAM = new Map([
 	["b", ["#a38b53", "Background"]],
 	["w", ["#000", "Walls"]]
 ]);
+Object.freeze(TEAM);
 
 const MAX_PH = 1920, MAX_PV = 1080;
 //--------------------------------------------------------------------HELPERS
@@ -44,6 +43,324 @@ function parseRangeCoords(rangeCoords) {
 }
 function createStrCoord(nums) {
 	return String.fromCharCode(64+nums[1]) + nums[0];
+}
+
+function scaleColour(_colour, _scale) {
+	let out = ["#"];
+	let di = (_colour.length < 6) ? 1 : 2;
+
+	for (let i = 0; i < 3; i++) {
+		let el = _colour.slice(i*di+1, (i+1)*di+1);
+		el = parseInt((di == 1) ? el+el : el, 16)*_scale;
+		el = ((el > 255) ? 255 : (el < 0) ? 0 : Math.floor(el)).toString(16)
+		out.push((el.length < 2) ? "0"+el : el);
+	}
+
+	return out.join("");
+}
+//--------------------------------------------------------------------TESTING
+//------------------------------------RECT
+class Rect {
+	constructor(_x, _y, _h, _v) {
+		this.setAbs(_x, _y, _h, _v);
+	}
+
+	setAbs(_x, _y, _h, _v) {
+		this.x = (_x > 0) ? _x : 0;
+		this.y = (_y > 0) ? _y : 0;
+		this.h = (_h > 0) ? _h : 1;
+		this.v = (_v > 0) ? _v : 1;
+	}
+	setPos(_x, _y) {
+		this.x = _x;
+		this.y = _y;
+		return this;
+	}
+	setDim(_h, _v) {
+		this.h = _h;
+		this.v = _v;
+		return this;
+	}
+	set(_x, _y, _h, _v) {
+		return this.setPos(_x, _y).setDim(_h, _v);
+	}
+	setFrom(_r) {
+		return this.set(_r.x, _r.y, _r.h, _r.v);
+	}
+
+	shunt(_x, _y) {
+		return this.setPos(_x+this.x, _y+this.y);
+	}
+	stretch(_h, _v) {
+		return this.setDim(_h+this.h, _v+this.v);
+	}
+
+	centerStretch(_h, _v) {
+		return this.setAbs(this.x + (this.h-_h)/2, this.y + (this.v-_v)/2, _h, _v);
+	}
+}
+//------------------------------------BRUSH
+class Brush extends Rect {
+	constructor(_ctx, _pS, _pM) {
+		super(_pM, _pM, _pS-2*_pM, _pS-2*_pM);
+		this.ctx = _ctx;
+		this.pS = _pS;
+		this.pM = _pM;
+	}
+
+	setPos(_x, _y) {
+		return super.setPos(_x*this.pS + this.pM, _y*this.pS + this.pM);
+	}
+	setDim(_h, _v) {
+		return super.setDim(_h*this.pS - 2*this.pM, _v*this.pS - 2*this.pM);
+	}
+
+	shunt(_x, _y) {
+		return super.shunt(_x*this.pS, _y*this.pS);
+	}
+	stretch(_h, _v) {
+		return super.stretch(_h*this.pS, _v*this.pS);
+	}
+
+	fill() {
+		this.ctx.fillRect(this.x, this.y, this.h, this.v);
+	}
+	write(_txt) {
+		this.ctx.fillText(_txt, this.x + this.h/2, this.y + this.v/2);
+	}
+	draw(_img) {
+		this.ctx.drawImage(_img, this.x, this.y, this.h, this.v);
+	}
+
+	reset() {
+		return this.setAbs(this.pM, this.pM, this.pS-2*this.pM, this.pS-2*this.pM);
+	}
+}
+
+class PaintStyle {
+	static GRID = -1;
+	static grid(_brush, _token, _args) {
+		_brush.ctx.fillStyle = _args.colour;
+		_brush.setDim(1, 1);
+		for (let y = _token.y; y < _token.y + _token.v; y++) {
+			for (let x = _token.x; x < _token.x + _token.h; x++) {
+				_brush.setPos(x, y);
+				_brush.fill();
+			}
+		}
+	}
+	static FILL = 1;
+	static fill(_brush, _token, _args) {
+		_brush.ctx.fillStyle = _args.colour;
+		_brush.setFrom(_token);
+		_brush.fill();
+		_brush.ctx.fillStyle = "#000";
+		_brush.write(_args.code + ((_args.many) ? (_args.i+1).toString() : ""));
+	}
+	static IMAGE = 0;
+	static async image(_brush, _token, _args) {
+		_brush.setFrom(_token);
+		try {
+			_brush.draw(await _args.img);
+			if (_args.many) {
+				_brush.ctx.fillStyle = _args.colour;
+				_brush.write((_args.i+1).toString());
+			}
+		} catch (e) {
+			PaintStyle.fill(_brush, _token, _args);
+		}
+	}
+	static NAMED_GRID = -2;
+	static namedGrid(_brush, _token, _args) {
+		PaintStyle.grid(_brush, _token, _args);
+		_args.colour = scaleColour(_args.colour, 0.75);
+		PaintStyle.fill(_brush,
+			new Rect(Math.floor(_token.x+_token.h/2), Math.floor(_token.y+_token.v/2), 1, 1),
+			_args
+		);
+	}
+
+	static getStyle(_code) {
+		switch (_code) {
+			case PaintStyle.FILL: return PaintStyle.fill;
+			case PaintStyle.IMAGE: return PaintStyle.image;
+			case PaintStyle.GRID: return PaintStyle.grid;
+			case PaintStyle.NAMED_GRID: return PaintStyle.namedGrid;
+
+			default: return PaintStyle.getStyle(0);
+		}
+	}
+}
+//------------------------------------TOKEN
+class Token extends Rect {
+	constructor(_dims, _pos, _visible = true) {
+		super(_pos[0], _pos[1], _dims[0], _dims[1]);
+		this.visible = _visible;
+		this.removed = false;
+	}
+}
+//------------------------------------TOKENGROUP
+class TokenGroup {
+	static makeCode(_name) {
+		return _name[0].toUpperCase()+_name[_name.length-1].toLowerCase();
+	}
+	constructor(_name, _styleCode, _dims, _colour) {
+		this.code = TokenGroup.makeCode(_name);
+		this.dims = _dims;
+		this.colour = _colour
+
+		this.styleCode = _styleCode;
+		this.style = PaintStyle.getStyle(_styleCode);
+
+		this.tokens = [];
+	}
+
+	push(_pos) {
+		this.tokens.push(new Token(this.dims, _pos));
+	}
+	pushMany(_posLs) {
+		for (const pos of _posLs) {this.push(pos);}
+	}
+
+	async paintAll(_brush, _imgUrls, _name) {
+		let args = {
+			colour: this.colour,
+			many: (this.tokens.length > 1),
+			code: this.code
+		};
+		if (this.styleCode === PaintStyle.IMAGE) {
+			let url = _imgUrls.get(_name);
+			if (url !== undefined) {args.img = loadImage(url);}
+			else {this.style = PaintStyle.fill;}
+		}
+		for (const [i, token] of this.tokens.entries()) {
+			if (token.visible && !token.removed) {
+				args.i = i;
+				await this.style(_brush, token, args);
+			}
+		}
+	}
+}
+//------------------------------------ARENA
+class Arena {
+	static recover(_arena) {
+		return (_arena === undefined) ? [false, false] : [_arena.map, _arena.img];
+	}
+	static toKeyCase(_str) {
+		return _str[0].toUpperCase() + _str.slice(1).toLowerCase();
+	}
+
+	constructor(_map, _img, _dims, _feedback = console.log, _feedhelp = "", _pH = MAX_PH, _pV = MAX_PV) {
+		this.dims = _dims;
+		this.feedhelp = _feedhelp;
+		this.feedback = _feedback; //void function(_feedhelp, string) => tells user what they did wrong
+		let pS = Math.floor(Math.min(_pH/_dims[0], _pV/_dims[1]));
+		let pM = Math.ceil(pS/64);
+
+		this.canvas = createCanvas((2+_dims[0])*pS, (2+_dims[1])*pS);
+		this.groups = new Map();
+
+		this.brush = new Brush(this.canvas.getContext("2d"), pS, pM);
+		this.brush.ctx.font = Math.floor(2*pS/3-4*pM) + "px Arial";
+		this.brush.ctx.textAlign = "center";
+		this.brush.ctx.textBaseline = "middle";
+
+		this.map = _map;
+		this.img = _img;
+	}
+
+	userFeedback(_str) {
+		return this.feedback(this.feedhelp, _str);
+	}
+
+	getGroup(_name) {
+		return this.groups.get(_name);
+	}
+	requireGroup(_name) {
+		let group = this.getGroup(_name);
+		if (group === undefined) {
+			this.userFeedback(`Group ${_name} does not exist.`);
+			return false;
+		}
+		return group;
+	}
+	getToken(_name, _i) {
+		let group = this.requireGroup(_name);
+		return (group) ? group.tokens[_i] : false;
+	}
+
+	addToGroup(_name, _pos) {
+		let group = this.requireGroup(_name);
+		if (group) {
+			if (_pos instanceof Array) {
+				(_pos[0] instanceof Array) ? group.pushMany(_pos) : group.push(_pos);
+			}
+			else {
+				this.userFeedback(`Position coords must be in form [x,y].`)
+			}
+		}
+	}
+	newGroup(_name, _styleCode, _colour, _dims = [1,1], _pos = false, _override = false) {
+		if (_override || this.groups.get(_name) === undefined) {
+			this.groups.set(_name, new TokenGroup(_name, _styleCode, _dims, _colour));
+			if (_pos) {this.addToGroup(_name, _pos);}
+		}
+		else {
+			this.userFeedback(`Group ${_name} already exists; allow override or use alternate name.`);
+		}
+	}
+
+	removeGroup(_name) {
+		this.groups.delete(_name);
+	}
+	removeToken(_name, _i, _andGroup = false) {
+		let group = this.requireGroup(_name);
+		if (group) {
+			group.tokens[i].removed = true;
+			if (_andGroup) {
+				for (const token of group.tokens) {
+					if (!token.removed) {_andGroup = false; break;}
+				}
+				if (_andGroup) {this.removeGroup(_name);}
+			}
+		}
+	}
+
+	async fetchImageUrls() {
+		if (!this.img) {return false;}
+		let out = new Map();
+		for (const [key, msg] of await this.img.messages.fetch({limit:100})) {
+			if (msg.attachments.size > 0) {
+				out.set(DaisyChar.toKeyCase(msg.content), msg.attachments.entries().next().value[1].url);
+			}
+		}
+		return out;
+	}
+	async prepMap() {
+		this.brush.ctx.fillStyle = "#000";
+		this.brush.setAbs(0, 0, this.canvas.width, this.canvas.height)
+		this.brush.fill();
+
+		this.brush.ctx.fillStyle = "#fff";
+		this.brush.reset();
+		for (const h of [0, this.dims[0]+1]) {
+			for (let v = 1; v <= this.dims[1]; v++) {this.brush.setPos(h,v).write(String.fromCharCode(64+v));}
+		}
+		for (const v of [0, this.dims[1]+1]) {
+			for (let h = 1; h <= this.dims[0]; h++) {this.brush.setPos(h,v).write(h.toString());}
+		}
+	}
+	async buildMap() {
+		let imgUrls = await this.fetchImageUrls();
+
+		this.prepMap();
+
+		for (const [name, group] of this.groups) {
+			await group.paintAll(this.brush, imgUrls, name);
+		}
+
+		return this.canvas.toBuffer("image/png");
+	}
 }
 //--------------------------------------------------------------------MAIN
 //------------------------------------CHAR
@@ -241,5 +558,7 @@ class DaisyMap {
 export {
 	TEAM,
 	DaisyMap,
-	DaisyChar
+	DaisyChar,
+	Arena,
+	PaintStyle
 }
