2cb7518be0bf6afeac8e405de59e7ea21b881c51
TDCalverley99 <TDCalverley99@gmail.com>
2023-02-24 13:12:53 +0000

    3.0.1-alpha.1: Add handling for TEST-bot & server hosting

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
 //--------------------------------------------------------------------INPUT
 
-const DEV_MODE = true;
+const DEV_STAGE = "ALPHA";
 
 //--------------------------------------------------------------------FINALIZE
+const DEV_MODE_SWITCH = {
+	"DEV": true,
+	"ALPHA": false,
+	"RELEASE": false
+};
+
 module.exports = require("./data/public-config.json");
-module.exports.dev_mode = DEV_MODE;
+module.exports.dev_mode = DEV_MODE_SWITCH[DEV_STAGE];
 
 const privateConfig = require("./data/private-config.json");
 for (const [key, val] of Object.entries(privateConfig.ALWAYS)) {
 	module.exports[key] = val;
 }
-for (const [key, val] of Object.entries(privateConfig[(DEV_MODE) ? "IF_DEV_MODE" : "IF_USE_MODE"])) {
+for (const [key, val] of Object.entries(privateConfig[DEV_STAGE])) {
 	module.exports[key] = val;
 }
